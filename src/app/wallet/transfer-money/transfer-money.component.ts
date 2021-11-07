b366b2e9601a91d901b663668e536c23ad75e3ae
limitofzero <limitofzero@gmail.com>
2021-11-07 19:30:31 +0300

    feat: add sending tx

--- src/app/wallet/transfer-money/transfer-money.component.ts
+++ src/app/wallet/transfer-money/transfer-money.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, ChangeDetectionStrategy, Injector } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Injector, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { TuiDialogService } from "@taiga-ui/core";
 import { PolymorpheusComponent } from "@tinkoff/ng-polymorpheus";
 import { SignTxComponent } from "./sign-tx/sign-tx.component";
+import { ActivatedRoute } from "@angular/router";
+import { WEB3 } from "../../../web3/web3.token";
+import Web3 from "web3";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-transfer-money',
@@ -13,7 +17,7 @@ import { SignTxComponent } from "./sign-tx/sign-tx.component";
 export class TransferMoneyComponent implements OnInit {
   public readonly form: FormGroup;
 
-  private readonly dialog = this.dialogService.open<number>(
+  private readonly dialog = this.dialogService.open<{ key: string }>(
     new PolymorpheusComponent(SignTxComponent, this.injector),
     {
       dismissible: true,
@@ -24,13 +28,18 @@ export class TransferMoneyComponent implements OnInit {
   constructor(
     private readonly fb: FormBuilder,
     private readonly dialogService: TuiDialogService,
-    private readonly injector: Injector
+    private readonly injector: Injector,
+    private readonly route: ActivatedRoute,
+    @Inject(WEB3) private readonly web3: Web3,
   ) {
     this.form = this.createForm();
   }
 
   ngOnInit(): void {
-
+    const from: string = this.route.parent?.parent?.snapshot.params['address'];
+    const fromController = this.form.get('from');
+    fromController?.setValue(from, { emitEvent: false });
+    fromController?.disable();
   }
 
 
@@ -43,19 +52,24 @@ export class TransferMoneyComponent implements OnInit {
   }
 
   private sendTransaction(): void {
-    this.dialog.subscribe({
-      next: data => {
-        console.log(data);
+    this.dialog.pipe(
+      switchMap(({ key }) => {
+        const formData = { ...this.form.getRawValue(), gasPrice: 1, nonce: 1 };
+        return this.web3.eth.accounts.signTransaction(formData, key);
+      })
+    ).subscribe({
+      next: (result) => {
+        console.log(result)
       },
-      complete: () => {
-        console.log('completed');
+      error: (err) => {
+        console.log(err);
       }
     })
   }
 
   private createForm(): FormGroup {
     return this.fb.group({
-      from: [],
+      from: ['', Validators.required],
       to: ['', Validators.required],
       value: ['', Validators.required],
       gas: [''],
